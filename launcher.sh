af02ef7895ad2b63e01ccb1c677158462acbf7a2
Lucie-Kin <lchauffo@student.42nice.fr>
2025-10-15 20:59:18 +0200

    README with link to preseed generator and all useful steps

--- launcher.sh
+++ launcher.sh
@@ -4,6 +4,13 @@ set -e
 # var
 USER="user"
 HOME="/home/$USER"
+TRANSFER="$HOME/combined"
+
+# check transfer file
+if [[ ! -d "$TRANSFER" ]]; then
+    echo "Please scp your files from host machine beforehand."
+    exit
+fi
 
 # add user to sudoers
 if ! sudo grep -q "^$USER ALL=(ALL:ALL) NOPASSWD:ALL" /etc/sudoers.d/$USER 2>/dev/null; then
@@ -13,10 +20,21 @@ fi
 
 # copy keys
 mkdir -p "$HOME/.ssh"
-cp /cdrom/id_rsa* "$HOME/.ssh"
+cp "$TRANSFER/id_rsa*" "$HOME/.ssh"
 chmod 600 "$HOME/.ssh/id_rsa"
 chmod 644 "$HOME/.ssh/id_rsa.pub"
 chown -R "$USER:$USER" "$HOME/.ssh"
+
+# add user to 'docker' group
+sudo usermod -aG docker "$USER"
+
+# copy .env
+mkdir -p "$HOME/tmp"
+chmod 755 "$HOME/tmp"
+if [[ -f "/cdrom/.env" ]]; then
+    cp "$TRANSFER/.env" "$HOME/tmp/.env"
+    chown "$USER:$USER" "$HOME/tmp/.env"
+fi
 
 # install vs-code
 sudo apt update
@@ -28,21 +46,16 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | 
 sudo apt update
 sudo apt install -y code
 
-# add user to 'docker' group
-sudo usermod -aG docker "$USER"
-
-# copy .env
-mkdir -p "$HOME/tmp"
-chmod 755 "$HOME/tmp"
-if [[ -f "/cdrom/.env" ]]; then
-    cp /cdrom/.env "$HOME/tmp/.env"
-    chown "$USER:$USER" "$HOME/tmp/.env"
-fi
-
 # clone git repo
-if [[ ! -d "$HOME/inception" ]]; then
+if [[ -d "$HOME/inception" ]]; then
     rm -rf "$HOME/inception"
     git clone git@vogsphere.42nice.fr:vogsphere/intra-uuid-a9e25ec3-ccf9-4b00-983c-15153ec3697f-6611255-lchauffo "$HOME/inception"
     sudo chown -R "$USER:$USER" "$HOME/inception"
+fi
+
+# destroy transfer directory
+if [[ -d "$TRANSFER" ]]; then
+    echo "All steps done. Destruction of directory."
+    rm -rf "$TRANSFER"
 fi
 
