68eec6d3559ce11f888218151896b05ee77b8110
Lucie-Kin <lchauffo@student.42nice.fr>
2025-10-15 22:18:19 +0200

    correction snap is named snapd for installation + retrying with firefox

--- launcher.sh
+++ launcher.sh
@@ -20,25 +20,32 @@ fi
 
 # copy keys
 mkdir -p "$HOME/.ssh"
-cp "$TRANSFER/id_rsa*" "$HOME/.ssh"
-chmod 600 "$HOME/.ssh/id_rsa"
-chmod 644 "$HOME/.ssh/id_rsa.pub"
-chown -R "$USER:$USER" "$HOME/.ssh"
+if [[ -f "$TRANSFER/id_rsa" ]]; then
+    echo "transfering ssh keys..."
+    mv "$TRANSFER/id_rsa" "$HOME/.ssh"
+    mv "$TRANSFER/id_rsa.pub" "$HOME/.ssh"
+    chmod 600 "$HOME/.ssh/id_rsa"
+    chmod 644 "$HOME/.ssh/id_rsa.pub"
+    chown -R "$USER:$USER" "$HOME/.ssh"
+fi
 
 # add user to 'docker' group
+echo "adding user to group 'docker'"
 sudo usermod -aG docker "$USER"
 
 # copy .env
 mkdir -p "$HOME/tmp"
+echo "transfering .env for my docker..."
 chmod 755 "$HOME/tmp"
-if [[ -f "/cdrom/.env" ]]; then
-    cp "$TRANSFER/.env" "$HOME/tmp/.env"
+if [[ -f "$TRANFER/.env" ]]; then
+    mv "$TRANSFER/.env" "$HOME/tmp/.env"
     chown "$USER:$USER" "$HOME/tmp/.env"
 fi
 
 # install vs-code
 sudo apt update
-sudo apt install -y wget gpg apt-transport-https
+sudo apt upgrade -y
+echo "installing vscode..."
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | \
   gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" | \
@@ -49,9 +56,10 @@ sudo apt install -y code
 # clone git repo
 if [[ -d "$HOME/inception" ]]; then
     rm -rf "$HOME/inception"
-    git clone git@vogsphere.42nice.fr:vogsphere/intra-uuid-a9e25ec3-ccf9-4b00-983c-15153ec3697f-6611255-lchauffo "$HOME/inception"
-    sudo chown -R "$USER:$USER" "$HOME/inception"
 fi
+echo "copying my work repo locally..."
+git clone git@vogsphere.42nice.fr:vogsphere/intra-uuid-a9e25ec3-ccf9-4b00-983c-15153ec3697f-6611255-lchauffo "$HOME/inception"
+sudo chown -R "$USER:$USER" "$HOME/inception"
 
 # destroy transfer directory
 if [[ -d "$TRANSFER" ]]; then
